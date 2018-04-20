84dc38198d0d4ff161999eec96d8aa16d007ddee
Sadra <ti9mail@gmail.com>
2018-04-20 11:28:41 +0200

    Add Spring structure

---
+++ src/main/java/de/sadrab/Mp16MypersonalToDoListUI/Mp16MyPersonalToDoListUiApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp16MypersonalToDoListUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp16MyPersonalToDoListUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp16MyPersonalToDoListUiApplication.class, args);
+	}
+}
